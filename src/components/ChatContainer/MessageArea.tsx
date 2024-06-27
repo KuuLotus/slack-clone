146206311f7ce736cffc6eb4f1e2b9a11d43960a
KuuLotus <kuulotus.dev@gmail.com>
2024-06-27 22:51:34 +0900

    [create] layout

---
+++ src/components/ChatContainer/MessageArea.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MessageArea = () => {
+  return (
+    <div className="flex-1 bg-gray-500">
+      {/* メッセージ表示エリア */}
+    </div>
+  );
+};
+
+export default MessageArea;
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="w-16 h-screen bg-gray-900"></div>
+  );
+};
+
+export default SideBar;
