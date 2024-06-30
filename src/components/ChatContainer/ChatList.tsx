d7c14161bbc91e058a098ad74e4265b3010ee5bd
KuuLotus <kuulotus.dev@gmail.com>
2024-06-30 23:32:32 +0900

    add channels

--- src/components/ChatContainer/ChatList.tsx
+++ src/components/ChatContainer/ChatList.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { ChannelRef } from "../../type/Channel";
 import { subscribeChannels } from "../../features/channel/channelAPI";
 import ChannelCell from "./ChannelCell";
+import ChannelAddModal from "./ChannelAddModal";
 
 const ChatList = () => {
   const [channelRefs, setChannelRefs] = useState<ChannelRef[]>([]);
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   useEffect(() => {
     const unsubscribe = subscribeChannels((channelRefs) => {
@@ -13,6 +15,14 @@ const ChatList = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="w-64 bg-gray-800">
       <div className="px-4 py-3 mb-4 border-b border-gray-700">
@@ -20,13 +30,17 @@ const ChatList = () => {
       </div>
       <div className="overflow-y-auto">
         {channelRefs.map(({ channel, id }) => (
-          <ChannelCell channel={channel} id={id} />
+          <ChannelCell key={id} channel={channel} id={id} />
         ))}
       </div>
       <div className="px-4 py-2">
-        <button className="text-gray-300 hover:text-">
+        <button
+          className="text-gray-300 hover:text-white"
+          onClick={handleOpenModal}
+        >
           + チャンネルを追加する
         </button>
+        {showModal && <ChannelAddModal handleCloseModal={handleCloseModal} />}
       </div>
     </div>
   );
